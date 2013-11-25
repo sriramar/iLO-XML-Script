439d44e1de913e9b3cd96b6c29cd7e7d4c13d2ab
sriramar <sriramar@hcl.com>
2013-11-25 19:50:38 +0530

    Sample iLO python script.
    The script uses twisted SSL module to query the iLO.

---
+++ ILO Base Script/ilo_query_modeler.py
@@ -0,0 +1,49 @@
+if __name__ == '__main__':
+    import ilo_query_modeler
+    raise SystemExit(ilo_query_modeler.main())
+
+import sys
+
+from twisted.internet.protocol import ClientFactory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import ssl, reactor
+
+def xml():
+    '''boilerplate xml body to send to the netapp filer'''
+    return (
+        '<?xml version=\"1.0\"?>'
+        '<RIBCL VERSION=\"2.21\">'
+        '<LOGIN USER_LOGIN=\"hcladmin\" PASSWORD=\"hcladmin\">'
+        '<SERVER_INFO MODE=\"read\">'
+        '<GET_EMBEDDED_HEALTH/>'
+        '</SERVER_INFO>'
+        '</LOGIN>'
+        '</RIBCL>'
+    )
+class ILOClient(LineReceiver):
+    end="/r/n"
+    def connectionMade(self):
+        self.sendLine(xml())
+        self.sendLine(self.end)
+
+    def connectionLost(self, reason):
+        print 'connection lost (protocol)'
+
+    def lineReceived(self, line):
+        print "receive:", line
+
+class ILOClientFactory(ClientFactory):
+    protocol = ILOClient
+
+    def clientConnectionFailed(self, connector, reason):
+        print 'connection failed:', reason.getErrorMessage()
+        reactor.stop()
+
+    def clientConnectionLost(self, connector, reason):
+        print 'connection lost:', reason.getErrorMessage()
+        reactor.stop()
+
+def main():
+    factory = ILOClientFactory()
+    reactor.connectSSL('10.100.71.26', 443, factory, ssl.CertificateOptions())
+    reactor.run()
